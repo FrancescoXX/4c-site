9046a8f6543e08aa7201abab1429e5ef725e77ee
FrancescoXX <francescociullaroma@gmail.com>
2021-09-01 09:03:47 +0200

    initial commit

---
+++ components/About.js
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <div className="py-6 bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-red-600 font-semibold tracking-wide uppercase">
+            what 4C stands for?
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Cool Community For Content Creators
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          Creating content can be rewarding, but it can also be a challenge to come up with ideas, get your content out there, and get it noticed. 
+          
+          </p>
+          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+          
+          Join our online community to connect with other like-minded creators, collaborate on ideas, and motivate each other to create and publish!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
