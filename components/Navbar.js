e8f2a410be7a40b6635d5e6a68a459b709a4d029
FrancescoXX <francescociullaroma@gmail.com>
2021-09-01 09:28:19 +0200

    orange test

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="bg-red-600">
+    <div className="bg-orange-600">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
