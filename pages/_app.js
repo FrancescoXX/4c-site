39e38e283ebdd91fe0d8e6e651ecc8e4e9738b05
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-09-01 19:08:53 -0400

    added GTM head script

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "../styles/global.css";
 import Head from 'next/head';
-
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
 		<div>
 			<Head>
+
+				{/* Google Tag Manager */}
+				<script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-XXXXXXX');`}}></script>
+				{/* Google Tag Manager */}
+
 				<meta name='description' content='4C: The Cool Community for Content Creators' />
 			</Head>
 			<Component {...pageProps} />;
