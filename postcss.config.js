cd017bf244695338e05cbaa727474340941d3e7d
Dan Spratling <dan@danspratling.dev>
2021-09-04 12:43:12 +0100

    Major Styling updates & Homepage changes
    Also
    
    Improves tailwind setup
    Standardises components & makes reusable
    Separates content
    
    TODO
    Add missing imagery
    Update fonts
    Add custom colours

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-// If you want to use other PostCSS plugins, see the following:
-// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {},
+    'postcss-nested': {},
     autoprefixer: {},
   },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: { ...colors },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
+};
