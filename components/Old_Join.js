0aa7eb1a1843bc8cb2ab96595e45c757c096e5e8
Dan Spratling <dan@danspratling.dev>
2021-09-04 12:43:12 +0100

    Major Styling updates & Homepage changes
    Also
    
    Improves tailwind setup
    Standardises components & makes reusable
    Separates content
    
    TODO
    Add missing imagery
    Update fonts
    Add custom colours

--- components/Old_Join.js
+++ components/Old_Join.js
@@ -1,21 +1,17 @@
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaPlayCircle } from 'react-icons/fa'
 const Join = () => {
   return (
-    <main className="py-6 bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <div className="inline-flex rounded-md shadow">
-            <a
-              href="https://discord.com/invite/TcmA2kbJeA"
-              className="inline-flex items-center justtify-center px-5 py-3 border border-transparent
-                text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-800"
-            >
-              <span className="px-1">
-                <FaDiscord />
-              </span>
-              Join the Community!
-            </a>
-          </div>
+    <main className='py-6 bg-white'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex gap-4'>
+          <a href='https://discord.com/invite/TcmA2kbJeA' className='btn btn-primary'>
+            <FaDiscord />
+            Join the Community!
+          </a>
+          <a href='https://discord.com/invite/TcmA2kbJeA' className='btn'>
+            <FaPlayCircle className='text-3xl text-orange-400' />
+            Why 4C?
+          </a>
         </div>
       </div>
     </main>
@@ -55,7 +51,7 @@ const Join = () => {
 
     //   </div>
     // </div>
-  );
-};
+  )
+}
 
-export default Join;
+export default Join
