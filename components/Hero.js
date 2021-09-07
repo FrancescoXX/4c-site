99ba1fd280aa9f6a1307094b24f3f094141ceb8f
Dan Spratling <dan@danspratling.dev>
2021-09-07 14:18:15 +0100

    Adds correct fonts & correct image for hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,17 @@
+import Image from "next/image";
+
 const Hero = ({ eyebrow, title, description, primaryLink, secondaryLink }) => {
   return (
     <section className="container">
-      <div className="grid md:grid-cols-2">
-        <div>
+      <div className="grid md:grid-cols-3 lg:grid-cols-2 items-center gap-12">
+        <div className="col-span-2 lg:col-span-1">
           <p className="text-sm text-orange-600 font-bold tracking-wide uppercase">
             {eyebrow}
           </p>
 
-          <p className="mt-2 text-5xl font-extrabold tracking-tight text-gray-900 md:text-6xl">
+          <h1 className="mt-2 text-5xl font-extrabold tracking-tight text-gray-900 md:text-6xl">
             {title}
-          </p>
+          </h1>
 
           <div className="prose-lg text-gray-500 my-8">{description}</div>
 
@@ -17,6 +19,9 @@ const Hero = ({ eyebrow, title, description, primaryLink, secondaryLink }) => {
             {primaryLink}
             {secondaryLink}
           </div>
+        </div>
+        <div className="hidden lg:block transform lg:scale-110 md:translate-x-24">
+          <Image src="/g10.png" alt="" width={1200} height={1369} />
         </div>
       </div>
     </section>
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&family=Red+Hat+Display:wght@700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
