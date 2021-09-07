95538672e138c950193f30c32e6ae36152c44760
Dan Spratling <dan@danspratling.dev>
2021-09-07 16:08:57 +0100

    Lint & Format (first run)

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang='en'>
-			<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
-
+export default MyDocument;
