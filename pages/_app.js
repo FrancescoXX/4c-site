1d5960da3973d69331d114a14d3f6cf07701d445
ProCode2 <iamprobablycoding@gmail.com>
2021-09-30 23:21:30 +0530

    feat: added google analytics via tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/global.css";
 import Head from "next/head";
+import Router from "next/router";
+import { useEffect } from "react";
+import { GTMPageView } from "../utils/gtm";
 
 function MyApp({ Component, pageProps }) {
+  // Initiate GTM
+  useEffect(() => {
+    const handleRouteChange = (url) => GTMPageView(url);
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
     <div>
       <Head>
