3f768eda81c6c72ab159f4dea2e10383983950b9
FrancescoXX <francescociullaroma@gmail.com>
2021-11-20 11:53:03 +0100

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// const GTM_ID = process.env.GTM_ID;
+const GTM_ID = process.env.GTM_ID;
 
 class MyDocument extends Document {
   render() {
@@ -14,12 +14,12 @@ class MyDocument extends Document {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTMID}');`,
+                })(window,document,'script','dataLayer','${GTM_ID}');`,
               }}
             />
             <noscript
               dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTMID}"
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
                 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
               }}
             />
