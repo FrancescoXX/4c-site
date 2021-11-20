76bf19c8339c7df3ca566f2e35d16d001440c81e
FrancescoXX <francescociullaroma@gmail.com>
2021-11-20 10:29:45 +0100

    gs env

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,12 @@ class MyDocument extends Document {
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');`,
+                })(window,document,'script','dataLayer','${GTMID}');`,
               }}
             />
             <noscript
               dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTMID}"
                 height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
               }}
             />
