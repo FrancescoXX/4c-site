31ed53e87bea39e26e35bff8d93bd2c6e2cbd7a0
Vishwa Gaurav <vishwagauravin@gmail.com>
2022-01-30 19:56:33 +0530

    bumped to tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: { ...colors },
     container: {
       center: true,
       padding: "1rem",
