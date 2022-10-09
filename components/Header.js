5a530b5e3b67103a2f48bb09849096bd9a062092
vigneshkanna <vkrishitaran@gmail.com>
2022-10-09 18:30:14 +0530

    change contributors to activemembers

--- components/Header.js
+++ components/Header.js
@@ -11,9 +11,9 @@ const Navbar = () => {
                 4C
               </button>
             </Link>
-            <Link href="/contributors">
+            <Link href="/activeMembers">
               <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Contributors
+                Active Members
               </button>
             </Link>
             <Link href="/projects">
