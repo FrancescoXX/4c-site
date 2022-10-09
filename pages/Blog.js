7bdfe702a90768f9f7a510df942268f1e701159a
Divv Saxena <111836493+DivvSaxena@users.noreply.github.com>
2022-10-09 21:19:56 +0530

    Create Blog.js

---
+++ pages/Blog.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Projects = () => {
+  return (
+    <Layout className="flex flex-col  items-center justify-center text-5xl ">
+      <div className="mt-40 font-bold text-white ">Blog</div>
+    </Layout>
+  );
+};
+
+export default Projects;
