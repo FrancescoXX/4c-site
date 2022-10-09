5ce73234a51e32ac0a8d0c8f5b92de6b16c57ddb
Einstein-Junior <adedejitobiloba7@gmail.com>
2022-10-09 15:26:04 +0100

    fixed logo routing

--- components/Header.js
+++ components/Header.js
@@ -6,14 +6,15 @@ const Navbar = () => {
       <div className="container">
         <nav className="flex items-start justify-between">
           <div className="flex flex-wrap items-center gap-6 md:gap-5">
-          <img
-            width="6%"
-            height="500"
-            frameBorder="0"
-            allowtransparency="true"
-            
-            src="https://raw.githubusercontent.com/FrancescoXX/4c-site/main/mediakit/4c-logo-circle.png"
-          ></img>
+            <Link href="/">
+              <img
+                className="max-w-[50px] w-[100%]"
+                frameBorder="0"
+                allowtransparency="true"
+                src="https://raw.githubusercontent.com/FrancescoXX/4c-site/main/mediakit/4c-logo-circle.png"
+              />
+            </Link>
+
             <Link href="/contributors">
               <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
                 Contributors
