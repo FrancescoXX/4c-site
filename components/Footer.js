6fb1798928c1b23fdabe1db8f59935d9378caed8
DevvSakib <devvsakib@gmail.com>
2022-10-13 22:40:50 +0600

    [FEATURE] FOOTER ADDED issue: #216 (#284)
    * videos button addded issue: #271
    
    * link updated to lowercase and hyphenated issues: #269
    
    * footer added

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="text-black text-center py-5 mt-10 bg-white">
+      <p>4c | Developed by 4C Community &amp; 2022</p>
+    </footer>
+  );
+};
+export default Footer;
