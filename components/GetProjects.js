9b1a238e3e973730888c5b4157ef491f68d4923c
DevvSakib <devvsakib@gmail.com>
2022-10-14 15:03:32 +0600

    projects card added

---
+++ components/GetProjects.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+// import contributors data
+import allProjects from "../content/projects.json";
+import imgTest from "../content/contributors.json";
+
+const GetProjects = () => {
+  return (
+    <div className="flex items-start justify-start mt-[3em]">
+      <section className="flex-1 text-center text-white">
+        <h1 className="text-4xl mb-10">Projects</h1>
+        <div className="flex flex-wrap items-center text-left justify-center pb-4 m-2 overflow-y-auto">
+          {allProjects.projects.map((item) => (
+            // test
+            // test
+            // test
+            // test
+            <a href={item.link}>
+              <div className="cursor-pointer items flex flex-col p-3  justify-between w-64 h-64 m-4 border bg-gray-300 shadow rounded-md hover:shadow-lg">
+                <div className="w-100 h-32 flex justify-center items-center shadow-sm text-center rounded-md bg-black">
+                  <p>IMG Preview</p>
+                </div>
+                <div className="text-black">
+                  <span className="mt-3 text-lg text-gray-800">
+                    Author: {item.name}
+                  </span>
+                  <h6 className="mb-2">Title: {item.title}</h6>
+                  <p className="hidden">Except: {item.description}</p>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default GetProjects;
