30d9d381f77eefb54f2890f3ae5a6b1bcbc50298
Njong Emy <81039882+Njong392@users.noreply.github.com>
2022-10-16 08:36:25 +0100

    ci: created releases.yml file for automatic releases (#290)
    * feat: added blog button to header
    
    * ci: added releases.yml file
    
    * chore(release): v0.1.0
    
    Co-authored-by: Conventional Changelog Action <conventional.changelog.action@github.com>
    Co-authored-by: Francesco Ciulla <francescociullaroma@gmail.com>

--- components/Header.js
+++ components/Header.js
@@ -32,6 +32,7 @@ const Navbar = () => {
               </button>
             </Link>
 
+
             <Link href="/blog">
               <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
                 Blog
@@ -43,12 +44,14 @@ const Navbar = () => {
                 Activities
               </button>
             </Link>
+
             <Link href="/videos">
               <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
                 Videos
 
               </button>
             </Link>
+
             <iframe
               src="https://ghbtns.com/github-btn.html?user=FrancescoXX&repo=4c-site&type=star&count=true&size=large"
               frameBorder="0"
