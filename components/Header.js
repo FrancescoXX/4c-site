9079efe80676066cdb2aa9b7dc1b27b83e75f52d
Arun9650 <arun02580@gmail.com>
2022-10-16 18:40:08 +0530

    responsive mobile

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
-
+import { useState } from "react";
 const Navbar = () => {
+  const [navbaropen, setNavbarOpen] = useState(false);
+
   return (
     <header className="p-[3em] my-[0] max-w-[940px]">
       <div className="container">
@@ -14,44 +16,73 @@ const Navbar = () => {
                 src="https://raw.githubusercontent.com/FrancescoXX/4c-site/main/mediakit/4c-logo-circle.png"
               />
             </Link>
-            <Link href="/activeMembers">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Active Members
-              </button>
-            </Link>
 
-            <Link href="/faq">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                FAQ
-              </button>
-            </Link>
+            <button
+              onClick={() => setNavbarOpen(!navbaropen)}
+              data-collapse-toggle="navbar-default"
+              type="button"
+              className=" inline-flex items-center  p-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
 
-            <Link href="/projects">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Projects
-              </button>
-            </Link>
+            <div
+              className={
+                "sm:flex flec-grow  gap-6 flex-wrap items-center" +
+                (navbaropen ? " flex" : " hidden")
+              }
+            >
+              <Link href="/activeMembers">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  Active Members
+                </button>
+              </Link>
 
+              <Link href="/faq">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  FAQ
+                </button>
+              </Link>
 
-            <Link href="/blog">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Blog
-              </button>
-            </Link>
+              <Link href="/projects">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  Projects
+                </button>
+              </Link>
 
-            <Link href="/activities">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Activities
-              </button>
-            </Link>
+              <Link href="/blog">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  Blog
+                </button>
+              </Link>
 
-            <Link href="/videos">
-              <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
-                Videos
+              <Link href="/activities">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  Activities
+                </button>
+              </Link>
 
-              </button>
-            </Link>
-
+              <Link href="/videos">
+                <button className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme">
+                  Videos
+                </button>
+              </Link>
+            </div>
           </div>
         </nav>
       </div>
