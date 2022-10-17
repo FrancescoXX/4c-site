dca8c3a55f1c995be19498f24ad3e43a238dbdbe
DevvSakib <devvsakib@gmail.com>
2022-10-17 11:23:49 +0600

    footer icon added

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,21 @@
+import { FaGithub, FaTwitter, FaDiscord } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className="text-black text-center py-5 bg-white">
-      <p>4c | Developed by 4C Community &amp; 2022</p>
+    <footer className="text-black flex flex-col items-center text-center py-5 bg-white">
+      <div className="flex items-center pb-3">
+        <div className="flex gap-10">
+          <a href="">
+            <FaGithub />
+          </a>
+          <a href="">
+            <FaDiscord />
+          </a>
+          <a href="">
+            <FaTwitter />
+          </a>
+        </div>
+      </div>
+      <p>4C | Developed by 4C Community &#169; 2022</p>
     </footer>
   );
 };
