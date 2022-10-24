70fd3ebb794b9f6aea024380416978c584592a6a
DevvSakib <devvsakib@gmail.com>
2022-10-24 20:39:15 +0600

    project added

--- components/GetProjects.js
+++ components/GetProjects.js
@@ -13,10 +13,7 @@ const GetProjects = () => {
             <a href={project.link}>
               <div className="cursor-pointer itemsGroup flex flex-col p-3  justify-between w-64 h-64 m-4 border bg-gray-300 shadow rounded-md hover:shadow-lg">
                 <div className="w-100 h-32 flex justify-center items-center shadow-sm text-center rounded-md bg-black">
-                  <img
-                    className="h-full w-full rounded-sm overflow-hidden object-contain"
-                    src={project.screenshot}
-                  />
+                  <img src={project.screenshot} />
                 </div>
                 <div className="text-black">
                   <span className="mt-3 text-md text-gray-800">
