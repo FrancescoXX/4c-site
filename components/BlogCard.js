4e178c83db4ae98cab146ea55d8174a9df0b766a
Yudai-creator <yudaipx77@gmail.com>
2022-11-14 10:37:41 -0500

    feat:easings for btn animations and blog card styles

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -2,12 +2,12 @@ import React from "react";
 import { FaTwitter, FaLink } from "react-icons/fa";
 import blogs from "../content/blog.json";
 
-const styles = "hover:text-gray-300 cursor-pointer";
+const styles = "transition-all ease-in hover:text-sky-700 cursor-pointer";
 const BlogCard = () => {
   return (
     <section className="flex flex-wrap items-center justify-center p-10 w-full">
       {blogs.blogs.map((blog) => (
-        <div className="text-white w-[330px]  xl:w-[380px] bg-gray-800 shadow-md p-5 m-5">
+        <div className="text-black  w-[330px]  xl:w-[380px] bg-[#f1f5f9] rounded-xl  shadow-md p-5 m-5">
           <div className="flex justify-between text-2xl">
             <a href={blog.twitter} target="_blank">
               <FaTwitter className={styles} />
@@ -21,14 +21,11 @@ const BlogCard = () => {
           </div>
           <a href={blog.link} target="_blank">
             <div>
-              <img
-                className="rounded-md w-max h-[200px]"
-                src={blog.screenshot}
-              />
+              <img className="rounded-md w-max" src={blog.screenshot} />
             </div>
           </a>
           <div>
-            <p className="text-[18px] text-gray-300 mt-5 leading-6">
+            <p className="text-[18px] text-black/80  mt-5 leading-6">
               {blog.description}
             </p>
           </div>
