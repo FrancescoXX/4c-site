e824c5cbcce1f0ec792055ca6ef2c9db1bc59e07
Tobiloba Adedeji <96334363+tobySolutions@users.noreply.github.com>
2022-12-02 21:35:33 +0100

    Revert "Fixed Videos embedding style"

--- components/GetVideos.js
+++ components/GetVideos.js
@@ -5,8 +5,8 @@ import allVideos from "../content/videos.json";
 
 const GetVideos = () => {
   const opts = {
-    height: "300px",
-    width: "500px",
+    height: "100%",
+    width: "100%",
     playerVars: {
       autoplay: 0,
     },
@@ -16,9 +16,9 @@ const GetVideos = () => {
       <section className="flex-1 text-center text-white">
         <h1 className="mb-5 text-4xl tracking-widest uppercase">Videos</h1>
         {/* <p className="text-white/70">Projects by 4c community Members</p> */}
-        <div className="flex flex-wrap items-center justify-center pb-4 m-2 overflow-y-auto">
+        <div className="flex flex-wrap items-center justify-center pt-4 my-[1rem] overflow-y-auto text-left gap-[1.5rem]">
           {allVideos.videos.map((video) => (
-            <div className="p-2 flex flex-col rounded-lg transition-all ease-in-out duration-200">
+            <div className="p-2 flex flex-col shrink rounded-lg group w-[280px] transition-all ease-in-out duration-200">
               <YouTube videoId={video.videoId} opts={opts} />
             </div>
           ))}
