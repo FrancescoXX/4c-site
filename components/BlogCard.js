6849fa95b2b7187756702968a9846ed9c46c77c5
tobySolutions <adedejitobiloba7@gmail.com>
2022-12-03 22:03:36 +0100

    pulling

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -6,8 +6,11 @@ const styles = "transition-all ease-in hover:text-sky-700 cursor-pointer";
 const BlogCard = () => {
   return (
     <section className="flex flex-wrap items-center justify-center p-10 w-full">
-      {blogs.blogs.map((blog) => (
-        <div className="text-black  w-[330px] h-[450px]  xl:w-[380px] bg-[#f1f5f9] rounded-xl  shadow-md p-5 m-5">
+      {blogs.blogs.map((blog, index) => (
+        <div
+          key={index}
+          className="text-black  w-[330px] h-[450px]  xl:w-[380px] bg-[#f1f5f9] rounded-xl  shadow-md p-5 m-5"
+        >
           <div className="flex justify-between text-2xl">
             <a href={blog.twitter} target="_blank">
               <FaTwitter className={styles} />
@@ -20,12 +23,10 @@ const BlogCard = () => {
             <h2 className="title text-2xl my-10">{blog.title}</h2>
           </div>
           <a href={blog.link} target="_blank">
-            <div className="w-[98%]">
-              <img
-                className="rounded-md w-fit h-40 object-cover"
-                src={blog.screenshot}
-              />
-            </div>
+            <img
+              className="rounded-md max-h-[20rem] object-contain"
+              src={blog.screenshot}
+            />
           </a>
           <div>
             <p className="text-[18px] text-black/80  mt-5 leading-6">
