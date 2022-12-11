38cac0d697f8369d245736a690db40f1453a82be
DevvSakib <devvsakib@gmail.com>
2022-12-11 23:43:19 +0600

    changed a tag to link

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import { BsX } from "react-icons/bs";
 import { useState, useEffect } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import Link from "next/link";
 
 // import navbar data
 import navbarData from "../content/navbar.json";
@@ -32,14 +33,14 @@ const Navbar = () => {
       <div className="container">
         <nav className="flex items-start justify-between">
           <div className="relative flex flex-wrap items-center justify-between gap-6 w-[100%]  ">
-            <a href="/">
+            <Link href="/">
               <img
                 className="w-[100%] max-w-[50px] hover:cursor-pointer"
                 frameBorder="0"
                 allowtransparency="true"
                 src="https://raw.githubusercontent.com/FrancescoXX/4c-site/main/mediakit/4c-logo-circle.png"
               />
-            </a>
+            </Link>
             <div
               className={`fixed lg:relative flex flex-col lg:flex-row p-2 bg-[#708fe6] lg:bg-transparent h-[100vh] lg:h-full top-0 lg:top-0 right-0 lg:right-0 all ease-in-out gap-6 transition delay-150  ${
                 menuActive ? "" : "hidden"
@@ -51,26 +52,26 @@ const Navbar = () => {
               >
                 <BsX />
               </button>
-              <ul className="flex flex-col items-center justify-center list-none gap-[2rem] lg:gap-[0.5rem] lg:flex-row">
+              <div className="flex flex-col items-center justify-center list-none gap-[2rem] lg:gap-[0.5rem] lg:flex-row">
                 {navItems.map((navItem, idx) => (
-                  <li key={idx}>
-                    <a
-                      href={navItem.href}
+                  <Link href={navItem.href} key={idx}>
+                    <span
                       onClick={() => {
                         window.localStorage.setItem("active", navItem.href);
                       }}
-                      className={`px-4 py-2 font-bold  rounded hov-bg-theme transition-all ease-out hover:-translate-y-1  ${
-                        activeButton == navItem.href
-                          ? "bg-[#708fe6] text-white"
-                          : "bg-white text-black"
-                      }`}
+                      className={`px-4 py-2 font-bold  rounded hov-bg-theme transition-all ease-out hover:-translate-y-1 hover:cursor-pointer
+                       ${
+                         activeButton == navItem.href
+                           ? "bg-[#708fe6] text-white"
+                           : "bg-white text-black"
+                       }`}
                       // className="px-4 py-2 font-bold text-black bg-white rounded hov-bg-theme transition-all ease-out hover:-translate-y-1"
                     >
                       {navItem.pageName}
-                    </a>
-                  </li>
+                    </span>
+                  </Link>
                 ))}
-              </ul>
+              </div>
             </div>
             <button
               className="text-xl hamburger"
