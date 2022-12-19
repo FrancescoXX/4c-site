1cf595a55c44a644db2b827a92186f47d929954d
DevvSakib <devvsakib@gmail.com>
2022-12-19 23:30:15 +0600

    video card design improve

--- components/GetVideos.js
+++ components/GetVideos.js
@@ -4,16 +4,16 @@ import allVideos from "../content/videos.json";
 
 const GetVideos = () => {
   return (
-    <div className="flex items-center justify-center mt-[3em]">
-      <section className="flex-1 text-center text-white max-w-bodyContainer">
-        <h1 className="mb-5 text-4xl tracking-widest uppercase">Videos</h1>
+    <div className="mt-[3em] flex items-center justify-center">
+      <section className="max-w-bodyContainer flex-1 text-center text-white">
+        <h1 className="mb-5 text-4xl uppercase tracking-widest">Videos</h1>
         <p className="text-white/70">Videos by 4c community Members</p>
         <div className="mx-4 lg:mx-12">
-          <div className="flex flex-wrap items-center justify-center w-full p-10">
+          <div className="flex w-full flex-wrap items-center justify-center p-10">
             {allVideos.videos.map((video, index) => (
               <div
                 key={index}
-                className="w-full p-6 bg-white rounded shadow cursor-pointer details bg-opacity-20 backdrop-blur-lg hov-bg-theme m-5 w-80 rounded-xl bg-[#f1f5f9]  p-5 text-white shadow-md transition-all ease-out hover:-translate-y-1 "
+                className="details hov-bg-theme my-4 w-full w-auto cursor-pointer rounded rounded-xl bg-white bg-[#f1f5f9] bg-opacity-20 p-4 text-white shadow shadow-md backdrop-blur-lg transition-all ease-out hover:-translate-y-1 sm:m-5 sm:w-80 sm:p-6 "
               >
                 <a
                   href={`https://www.youtube.com/watch?v=${video.videoId}`}
@@ -25,7 +25,7 @@ const GetVideos = () => {
                     alt=""
                   />
                   <div className="text-left">
-                    <h2 className="text-lg">{video.videoTitle}</h2>
+                    <h2 className="text-sm sm:text-lg">{video.videoTitle}</h2>
                     <span className="text-sm font-thin text-gray-200">
                       {video.videoAuthor}
                     </span>
