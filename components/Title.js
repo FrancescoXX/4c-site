e80061bde15fa4a8247ec4ddaaadb7ff92a74b19
MadhuSaini22 <madhusaini46810@gmail.com>
2022-12-22 21:45:07 +0530

    changes

--- components/Title.js
+++ components/Title.js
@@ -3,11 +3,11 @@ import React from "react";
 function Title({ heading }) {
   // Main Heading component
   return (
-    <div>
+    <>
       <h1 className="headTitle mb-5 uppercase text-white tracking-widest">
         {heading}
       </h1>
-    </div>
+    </>
   );
 }
 
