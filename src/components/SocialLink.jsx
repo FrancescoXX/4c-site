4a53acf31109f79ec7661620ad774d556a76ee2c
Aryan Babber <86878236+AryanBabber@users.noreply.github.com>
2023-01-12 15:54:28 +0530

    UI changes in active members and staff

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const SocialLink = ({ link, Icon, label }) => (
-  <a href={link} target="_blank" rel="noopener noreferrer">
+  <a href={link} target="_blank" rel="noopener noreferrer" className="text-white hover:text-slate-300">
     <Icon className="text-2xl" />
     <span className="sr-only">{label}</span>
   </a>
