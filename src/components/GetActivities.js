505a6078840804df630191c054252f353aed06cf
DevvSakib <devvsakib@gmail.com>
2023-01-18 13:18:13 +0600

    fix: activities card width fixed for md lg xl device activities card width fixed for md lg xl device

--- src/components/GetActivities.js
+++ src/components/GetActivities.js
@@ -8,11 +8,11 @@ const styles = "transition-all ease-in cursor-pointer"
 const GetActivities = () => (
   <div className="flex flex-col items-center">
     <Title heading="Activities" />
-    <section className="flex w-full flex-wrap items-center justify-center p-4 sm:p-10">
+    <section className="flex justify-center sm:w-[500px] xl:w-auto  p-4 sm:p-10">
       {events.map((event, idx) => {
         return (
           event.isActive && (
-            <div key={idx} className='hov-bg-theme m-5 h-auto rounded-xl bg-[#f1f5f9] p-5 text-black shadow-md relative transition-all ease-out xl:w-[380px]'>
+            <div key={idx} className='hov-bg-theme m-5  h-auto rounded-xl bg-[#f1f5f9] p-5 text-black shadow-md relative transition-all ease-out xl:w-[380px]'>
               <div>
                 <h2 className="title mt-5 text-2xl">{event.name}</h2>
                 <div className="flex justify-between flex-col sm:flex-row items-start sm:items-center w-full">
