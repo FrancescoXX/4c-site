dd6c43f6c92f7e2ef1b2101c1aa863b2d4b5adfc
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-03-25 21:00:51 +0530

    chore: code formatted

--- src/components/GetActivities.js
+++ src/components/GetActivities.js
@@ -12,22 +12,39 @@ const GetActivities = () => (
       {events.map((event, idx) => {
         return (
           event.isActive && (
-            <div key={idx} className='hov-bg-theme m-5  h-auto rounded-xl bg-[#f1f5f9] p-5 text-black shadow-md relative transition-all ease-out xl:w-[380px]'>
+            <div
+              key={idx}
+              className="hov-bg-theme m-5  h-auto rounded-xl bg-[#f1f5f9] p-5 text-black shadow-md relative transition-all ease-out xl:w-[380px]"
+            >
               <div>
                 <h2 className="title mt-5 text-2xl">{event.name}</h2>
                 <div className="w-full">
-                  <p className="text-md  text-[14px] text-gray-600 leading-[20px] mt-3">By {event.organizer} <span className="block sm:inline text-sm font-[600] text-black">{event.date}</span> </p>
+                  <p className="text-md  text-[14px] text-gray-600 leading-[20px] mt-3">
+                    By {event.organizer}{" "}
+                    <span className="block sm:inline text-sm font-[600] text-black">
+                      {event.date}
+                    </span>{" "}
+                  </p>
                 </div>
-                <img src={event?.image} alt={event.name} className='w-full mb-4 rounded-md object-contain my-4' />
+                <img
+                  src={event?.image}
+                  alt={event.name}
+                  className="w-full mb-4 rounded-md object-contain my-4"
+                />
 
                 <div className="flex justify-between text-2xl  bottom-1 w-[100%]">
-                  <a href={event.link} target='_blank' className="bg-theme my-4 rounded-md flex h-14 w-full items-center justify-center bg-[#70b7e6] text-4xl text-white" rel="noreferrer">
+                  <a
+                    href={event.link}
+                    target="_blank"
+                    className="bg-theme my-4 rounded-md flex h-14 w-full items-center justify-center bg-[#70b7e6] text-4xl text-white"
+                    rel="noreferrer"
+                  >
                     {event.venue === "Twitter"
                       ? (
-                        <FaTwitter className={styles} />
+                      <FaTwitter className={styles} />
                         )
                       : (
-                        <FaDiscord className={styles} />
+                      <FaDiscord className={styles} />
                         )}
                   </a>
                 </div>
