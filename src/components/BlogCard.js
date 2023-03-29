d96b19a023b9fa59b5dbdc4ba7e329a113ffff0d
Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>
2023-03-29 18:30:53 +0530

    fix: Update BlogCard.js

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -12,7 +12,7 @@ const BlogCard = () => {
           key={index}
           className="hov-bg-theme m-5 h-[510px] w-[330px] rounded-xl bg-[#f1f5f9] p-5 text-black shadow-md transition-all ease-out hover:-translate-y-1 xl:w-[380px]"
         >
-          <div className="flex justify-between text-2xl">
+          <article className="flex justify-between text-2xl">
             <a href={blog.twitter} target="_blank" rel="noreferrer">
               <FaTwitter className={styles} />
             </a>
@@ -32,11 +32,11 @@ const BlogCard = () => {
               width={568.87}
             />
           </a>
-          <div>
+          <article>
             <p className="mt-3 text-[18px] leading-6 text-black/80 line-clamp-2">
               {blog.description}
             </p>
-          </div>
+          </article>
         </div>
       ))}
     </section>
