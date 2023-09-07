ae7ab8200ab3763bb562a04620e8e5a1cf3ed734
tobySolutions <adedejitobiloba7@gmail.com>
2023-09-07 20:01:33 +0100

    add million

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import million from 'million/compiler';
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
@@ -16,4 +18,13 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const millionConfig = {
+  auto: true,
+  // if you're using RSC:
+  // auto: { rsc: true },
+}
+ 
+
+
+export default million.next(nextConfig, millionConfig);
+
