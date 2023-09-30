03a252e5ad15ef86838accb7de6a59b3e8374a2f
DESKTOP-523C8BN\Madhu <madhusaini46810@gmail.com>
2023-09-30 15:16:40 +0530

    refact:staff list

--- src/data/staff.js
+++ src/data/staff.js
@@ -1,22 +1,17 @@
-import JaxAvatar from "assets/sources/staff/44jax44.webp"
-import AliRezaAvatar from "assets/sources/staff/AliReza1083.webp"
-import ArdaAvatar from "assets/sources/staff/ardasevinc.webp"
-import AvieAvatar from "assets/sources/staff/avie-dev.webp"
-import AvneeshAvatar from "assets/sources/staff/avneesh0612.webp"
-import ShubhangiAvatar from "assets/sources/staff/codewithshubhi.webp"
-import DunsinAvatar from "assets/sources/staff/Dun-sin.webp"
-import FrancescoAvatar from "assets/sources/staff/FrancescoXX.webp"
-import GrahamAvatar from "assets/sources/staff/InHuOfficial.webp"
-import KrupaliAvatar from "assets/sources/staff/krupalitrivedi.webp"
-import LouellaAvatar from "assets/sources/staff/lovelacecoding.webp"
-import MadhuAvatar from "assets/sources/staff/MadhuSaini22.webp"
-import NaomiAvatar from "assets/sources/staff/naomi-lgbt.webp"
-import EmyAvatar from "assets/sources/staff/Njong392.webp"
-import PradumnaAvatar from "assets/sources/staff/Pradumnasaraf.webp"
-import SumitAvatar from "assets/sources/staff/sumitsaurabh927.webp"
-import KyleAvatar from "assets/sources/staff/TechSquidTV.webp"
-import TobiAvatar from "assets/sources/staff/tobySolutions.webp"
-import YudaiAvatar from "assets/sources/staff/Yudai-creator.webp"
+import JaxAvatar from "assets/sources/staff/44jax44.webp";
+import AliRezaAvatar from "assets/sources/staff/AliReza1083.webp";
+import AvieAvatar from "assets/sources/staff/avie-dev.webp";
+import DunsinAvatar from "assets/sources/staff/Dun-sin.webp";
+import FrancescoAvatar from "assets/sources/staff/FrancescoXX.webp";
+import GrahamAvatar from "assets/sources/staff/InHuOfficial.webp";
+import KrupaliAvatar from "assets/sources/staff/krupalitrivedi.webp";
+import LouellaAvatar from "assets/sources/staff/lovelacecoding.webp";
+import MadhuAvatar from "assets/sources/staff/MadhuSaini22.webp";
+import PradumnaAvatar from "assets/sources/staff/Pradumnasaraf.webp";
+import SumitAvatar from "assets/sources/staff/sumitsaurabh927.webp";
+import TobiAvatar from "assets/sources/staff/tobySolutions.webp";
+import ElderAvatar from "assets/sources/staff/Elder.webp";
+import TiyaAvatar from "assets/sources/staff/Tiya.webp";
 
 /**
  * @typedef {Object} Profile
@@ -27,7 +22,7 @@ import YudaiAvatar from "assets/sources/staff/Yudai-creator.webp"
  * @property {string?} twitter
  * @property {string?} blogUrl
  * @property {number?} contributions
-*/
+ */
 
 /**
  * @type {Profile[]}
@@ -43,19 +38,11 @@ const staff = [
     contributions: 0,
   },
   {
-    name: "Avneesh",
-    avatar: AvneeshAvatar,
-    github: "avneesh0612",
-    description: "15 | Full stack web3 developer | Developer Advocate|@thirdweb| Member @developer_dao| 0xavneesh.eth | avneesh.sol | avneesh.lens",
-    twitter: "avneesh0612",
-    blogUrl: "",
-    contributions: 0,
-  },
-  {
     name: "Ali Reza",
     avatar: AliRezaAvatar,
     github: "AliReza1083",
-    description: "Passionate in Coding, Refugee Boy, Threads about Web Development",
+    description:
+      "Passionate in Coding, Refugee Boy, Threads about Web Development",
     twitter: "Ali_Developer05",
     blogUrl: "https://linkfree.eddiehub.io/AliReza1083",
     contributions: 0,
@@ -64,7 +51,8 @@ const staff = [
     name: "Avie",
     avatar: AvieAvatar,
     github: "avie-dev",
-    description: "BrSE in Japan(YNS)🌸 | Community leader@ http://ossph.org | Community manager| @4ccommunityhq| open source | Spaces Host | Sharing about Tech💻, Japan🇯🇵 & Art",
+    description:
+      "BrSE in Japan(YNS)🌸 | Community leader@ http://ossph.org | Community manager| @4ccommunityhq| open source | Spaces Host | Sharing about Tech💻, Japan🇯🇵 & Art",
     twitter: "AvieDev",
     blogUrl: "",
     contributions: 0,
@@ -73,7 +61,8 @@ const staff = [
     name: "Dunsin",
     avatar: DunsinAvatar,
     github: "Dun-sin",
-    description: "🧑‍💻18|⚙Building https://code-magic.vercel.app and https://whischat.vercel.app |☯Being real about stuff |🥑Open Source |🎖️Tweeting from my POV |🧒She/Her",
+    description:
+      "🧑‍💻18|⚙Building https://code-magic.vercel.app and https://whischat.vercel.app |☯Being real about stuff |🥑Open Source |🎖️Tweeting from my POV |🧒She/Her",
     twitter: "DunsinWebDev",
     blogUrl: "https://linktr.ee/DunsinCodes",
     contributions: 0,
@@ -82,7 +71,8 @@ const staff = [
     name: "Francesco Ciulla",
     avatar: FrancescoAvatar,
     github: "FrancescoXX",
-    description: "·Follow me, I will help you with Web3 & DevRel· Building a 1M Community 19%· Docker Captain |@dailydotdev · Public Speaker· 4C founder @4ccommunityhq",
+    description:
+      "·Follow me, I will help you with Web3 & DevRel· Building a 1M Community 19%· Docker Captain |@dailydotdev · Public Speaker· 4C founder @4ccommunityhq",
     twitter: "FrancescoCiull4",
     youtube: "https://www.youtube.com/c/FrancescoCiulla",
     twitch: "https://www.twitch.tv/francesco_ciulla",
@@ -93,18 +83,10 @@ const staff = [
     name: "JAX",
     avatar: JaxAvatar,
     github: "44jax44",
-    description: "I will help you become a better communicator | Tweets on all aspects of communication| Life Health & Fitness Coach | Can communicate in 🇬🇧 🇫🇷 🇱🇧 🇪🇸 🇵🇹",
+    description:
+      "I will help you become a better communicator | Tweets on all aspects of communication| Life Health & Fitness Coach | Can communicate in 🇬🇧 🇫🇷 🇱🇧 🇪🇸 🇵🇹",
     twitter: "44jax44",
     blogUrl: "https://linktr.ee/44jax44",
-    contributions: 0,
-  },
-  {
-    name: "Naomi",
-    avatar: NaomiAvatar,
-    github: "naomi-lgbt",
-    description: "she / her 🏳‍⚧ Code Princess, Community Queen, Magical Girl Champion, VTuber goddess",
-    twitter: "naomi_lgbt",
-    blogUrl: "https://www.naomi.lgbt/",
     contributions: 0,
   },
   {
@@ -117,67 +99,33 @@ const staff = [
     contributions: 0,
   },
   {
-    name: "Shubhangi",
-    avatar: ShubhangiAvatar,
-    github: "codewithshubhi",
-    description: "Software Engineer • Open Source • Designer and Developer • Technical writer",
-    twitter: "heyShubhi",
-    blogUrl: "",
-    contributions: 0,
-  },
-  {
     name: "Sumit",
     avatar: SumitAvatar,
     github: "sumitsaurabh927",
-    description: "Explaining tech the way I wish I was taught | Tweets on Javascript, React, Open Source, and Web Development |🎙 Spaces | 🥑 Open Source advocate",
+    description:
+      "Explaining tech the way I wish I was taught | Tweets on Javascript, React, Open Source, and Web Development |🎙 Spaces | 🥑 Open Source advocate",
     twitter: "sumitsaurabh927",
     blogUrl: "https://sumitsaurabh.hashnode.dev/",
-    contributions: 0,
-  },
-  {
-    name: "Kyle TechSquidTV",
-    avatar: KyleAvatar,
-    github: "TechSquidTV",
-    description: "🎥Tech YouTuber. Staff SDE @CircleCI. makes cool open-source stuff.http://techsquidtv.com",
-    twitter: "TechSquidTV",
-    youtube: "https://www.youtube.com/techsquidtv",
-    blogUrl: "https://techsquidtv.com/",
     contributions: 0,
   },
   {
     name: "Tobiloba Adedeji",
     avatar: TobiAvatar,
     github: "tobySolutions",
-    description: "Frontend Engineer  | YouTuber | Communicator | Accessibility Advocate | Open Source Enthusiast. Software Engineer Intern Nithub,  Staff  @4ccommunityhq",
+    description:
+      "Frontend Engineer  | YouTuber | Communicator | Accessibility Advocate | Open Source Enthusiast. Software Engineer Intern Nithub,  Staff  @4ccommunityhq",
     twitter: "toby_solutions",
     blogUrl: "",
     linkedin: "tobiloba-adedeji",
     contributions: 34,
   },
   {
-    name: "Yudai",
-    avatar: YudaiAvatar,
-    github: "Yudai-creator",
-    description: "Code | Design | Buildin in Public 🚀",
-    twitter: "creator_yudai",
-    blogUrl: "https://yudaicreator.com/",
-    contributions: 0,
-  },
-  {
-    name: "Emy",
-    avatar: EmyAvatar,
-    github: "Njong392",
-    twitter: "njong_emy",
-    description: "Ambassador at GitKraken | Technical Writer at LogRocket | Front end developer 💻 | Open Source enthusiast 💥| Spaces host 🎙| dev advocate wannabe 🥑",
-    blogUrl: "https://linkfree.eddiehub.io/Njong392",
-    contributions: 0,
-  },
-  {
     name: "Louëlla Creemers",
     avatar: LouellaAvatar,
     github: "lovelacecoding",
     linkedin: "louelladev",
-    description: "Web Developer 🌍 | Public Speaker 🗣️ | .NET & C# 🖥️  | Dad Joke Enthusiast 🤓 | Learning and Joking With You While Working and Studying CS",
+    description:
+      "Web Developer 🌍 | Public Speaker 🗣️ | .NET & C# 🖥️  | Dad Joke Enthusiast 🤓 | Learning and Joking With You While Working and Studying CS",
     twitter: "lovelacecoding",
     twitch: "https://www.twitch.tv/lovelacecoding",
     blogUrl: "https://lovelacecoding.hashnode.dev/",
@@ -187,15 +135,16 @@ const staff = [
     name: "Madhu Saini",
     avatar: MadhuAvatar,
     github: "MadhuSaini22",
-    description: "Enthusiastic Open-Source Developer | MERN Stack | Student",
+    description: "Full Stack Developer | Open Source Enthusiast",
     twitter: "MadhuSaini22",
-    blogUrl: "https://madhusaini.netlify.app/",
+    blogUrl: "https://madhusaini46810.wixsite.com/original",
     linkedin: "madhu-saini",
     contributions: 24,
   },
   {
     name: "Krupali",
-    description: "20 🚀 || talking about blogging and communities|| web3 and web dev learner | summarising spaces in threads | chillin' in discord servers |books 💜",
+    description:
+      "20 🚀 || talking about blogging and communities|| web3 and web dev learner | summarising spaces in threads | chillin' in discord servers |books 💜",
     avatar: KrupaliAvatar,
     github: "krupalitrivedi",
     twitter: "chai_really",
@@ -204,16 +153,27 @@ const staff = [
     contributions: null,
   },
   {
-    name: "Arda Sevinç",
-    description: "Generalist software dev and student, Tweets about web2, web3 and everything in between, Developer Dao #6095 at AlchemyLearn | EarnWeb3DAO",
-    avatar: ArdaAvatar,
-    github: "ardasevinc",
-    twitter: "ardasevinc_4",
-    blogUrl: "https://www.ardasevinc.com/",
-    contributions: 0,
+    name: "K. Eldreth aka Elder",
+    github: "",
+    description: "Just another bystander; Lead Dev in robotics",
+    twitter: "Elder_Youth",
+    blogUrl: "",
+    avatar: ElderAvatar,
+    linkedin: "",
+    contributions: null,
   },
-].sort((a, b) => (a.name > b.name ? 1 : -1))
+  {
+    name: "Tiya Bansal",
+    github: "Tiyabansal",
+    description: "SWE Intern @JPMC, UK | Cloud and Devops | IT Undergraduate",
+    twitter: "TiyaTwts",
+    blogUrl: "",
+    avatar: TiyaAvatar,
+    linkedin: "tiya-bansal",
+    contributions: null,
+  },
+].sort((a, b) => (a.name > b.name ? 1 : -1));
 
-export default staff
+export default staff;
 
 // module.exports = staff
